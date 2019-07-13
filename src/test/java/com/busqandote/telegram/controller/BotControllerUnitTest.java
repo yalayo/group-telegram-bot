ddb0e3e0cfa71e54d4db89de19638e461cf1f9c9
Busqandote <busqandote@gmail.com>
2019-07-13 16:05:38 -0700

    Creating object to be tested and first test.

--- src/test/java/com/busqandote/telegram/controller/BotControllerUnitTest.java
+++ src/test/java/com/busqandote/telegram/controller/BotControllerUnitTest.java
@@ -1,4 +1,14 @@
 package com.busqandote.telegram.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BotControllerUnitTest {
+    private BotController botController = new BotController();;
+
+    @Test
+    @DisplayName("If the message to be send is empty then return an error")
+    public void testMessageToSendEmpty() {
+
+    }
 }
