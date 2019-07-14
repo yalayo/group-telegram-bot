137ed59333149f349eac78d176a1167074290ee0
Busqandote <busqandote@gmail.com>
2019-07-13 17:49:52 -0700

    Implementing method in order to pass the tests.

--- src/main/java/com/busqandote/telegram/BotController.java
+++ src/main/java/com/busqandote/telegram/BotController.java
@@ -1,7 +1,53 @@
 package com.busqandote.telegram;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+
+import java.io.IOException;
+
 public class BotController {
+    private HttpClient httpClient;
+
+    public BotController() {}
+
     public boolean send(String message, String user) {
-        return false;
+        if(message.isEmpty())
+            return false;
+
+        String base = "https://api.telegram.org/bot";
+        String token = "";
+        String method = "";
+
+        String url = base + token + method;
+        HttpPost httpPost = new HttpPost(url);
+
+        String json = "{chat_id:" + user + ","
+        + "text: " + message + ","
+                + "}";
+
+        try {
+            StringEntity entity = new StringEntity(json);
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            HttpResponse response = httpClient.execute(httpPost);
+            StatusLine statusLine = response.getStatusLine();
+
+            if(statusLine.getStatusCode() != 200)
+                return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void setHttpClient(HttpClient httpClient) {
+        this.httpClient = httpClient;
     }
 }
