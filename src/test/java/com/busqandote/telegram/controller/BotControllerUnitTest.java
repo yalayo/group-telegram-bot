13da5703a75051a17b54ef1d5b1a8b3d00d12fa9
Busqandote <busqandote@gmail.com>
2019-07-13 17:20:53 -0700

    Working on test for the case when the message is not empty.

--- src/test/java/com/busqandote/telegram/controller/BotControllerUnitTest.java
+++ src/test/java/com/busqandote/telegram/controller/BotControllerUnitTest.java
@@ -1,14 +1,23 @@
 package com.busqandote.telegram.controller;
 
 import com.busqandote.telegram.BotController;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BotControllerUnitTest {
-    private BotController botController = new BotController();;
+    private BotController botController = new BotController();
 
     @Test
     @DisplayName("If the message to be send is empty then return false")
@@ -22,6 +31,23 @@ public class BotControllerUnitTest {
     public void testSendMessage() {
         String user = "test";
         String message = "Test message";
+
+        HttpClient httpClient = mock(HttpClient.class);
+        HttpHost httpHost = mock(HttpHost.class);
+        HttpRequest httpRequest = mock(HttpRequest.class);
+        HttpResponse httpResponse = mock(HttpResponse.class);
+        StatusLine statusLine = mock(StatusLine.class);
+
+        try {
+            when(httpClient.execute(httpHost, httpRequest)).thenReturn(httpResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        when(httpResponse.getStatusLine()).thenReturn(statusLine);
+        when(statusLine.getStatusCode()).thenReturn(200);
+
+        botController.setHttpClient(httpClient);
 
         assertTrue(botController.send(message, user));
     }
